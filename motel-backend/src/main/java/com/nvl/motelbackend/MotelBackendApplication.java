c916a8047a67cdc5ceb0ccf39d810aa4fa6bcf28
Lam <lamnguyenit212@gmail.com>
2023-03-16 20:12:46 +0700

    project init

---
+++ motel-backend/src/main/java/com/nvl/motelbackend/MotelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nvl.motelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotelBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotelBackendApplication.class, args);
+    }
+
+}
---
+++ motel-backend/src/test/java/com/nvl/motelbackend/MotelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvl.motelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
